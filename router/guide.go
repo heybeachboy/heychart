e07b7976ac775ed080e2d83a4cdff2849a260dd8
zhouletianvip6 <zhouletian1234@live.com>
2018-09-29 18:18:20 +0800

    gogo

--- router/guide.go
+++ router/guide.go
@@ -2,18 +2,11 @@ package router
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/robvdl/pongo2gin"
 	"heychart/controller"
 )
 
 func RegisterRoute(route *gin.Engine) {
-	opt := pongo2gin.RenderOptions{
-		TemplateDir: "templates/",
-		//TemplateDir: "E:/linus/code/gitlab/vesyncmqtt/src/templates/",
-	}
-
-	route.HTMLRender = pongo2gin.New(opt)
-	route.GET("/",controller.TestFunction)
+	 route.GET("/",controller.TestFunction)
 
 
 }
