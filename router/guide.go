39dfb92f16d13092a77a3540310a6e868ed571a2
zhouletianvip6 <zhouletian1234@live.com>
2018-09-29 21:43:38 +0800

    add acrross

--- router/guide.go
+++ router/guide.go
@@ -3,10 +3,14 @@ package router
 import (
 	"github.com/gin-gonic/gin"
 	"heychart/controller"
+	"github.com/gin-contrib/cors"
 )
 
 func RegisterRoute(route *gin.Engine) {
 	 route.LoadHTMLFiles("template/**/*")
+	 cfg := cors.DefaultConfig()
+	 cfg.AllowAllOrigins = true
+	 route.Use(cors.Default())
 	 route.GET("/",controller.Home)
 	 route.GET("/ws",controller.WebsocketServer)
 	// route.GET("/",controller.TestFunction)
